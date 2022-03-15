fe5763f6455ab62a420cdd65dedf6042a886dc7d
pplim <ppm404@inlumine.ual.es>
2022-03-15 19:25:45 +0100

    Initial commit

---
+++ cajaOpiniones.js
@@ -0,0 +1,23 @@
+const http=require('http');
+const url=require('url');
+const fs=require('fs');
+const querystring = require('querystring');
+
+const mime = {
+    'html' : 'text/html',
+    'css'  : 'text/css',
+    'jpg'  : 'image/jpg',
+    'ico'  : 'image/x-icon',
+    'mp3'  : 'audio/mpeg3',
+    'mp4'  : 'video/mp4'
+};
+
+const servidor=http.createServer((pedido ,respuesta) => {
+    const objetourl = url.parse(pedido.url);
+    let camino='public'+objetourl.pathname;
+    if (camino=='public/')
+        camino='public/index.html';
+    encaminar(pedido,respuesta,camino);
+});
+
+servidor.listen(8888);
