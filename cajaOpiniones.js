02e3b232f03b662b2866baf05b384461f7c2abbc
pplim <ppm404@inlumine.ual.es>
2022-03-16 19:53:45 +0100

    node.js

--- cajaOpiniones.js
+++ cajaOpiniones.js
@@ -1,3 +1,4 @@
+
 const http=require('http');
 const url=require('url');
 const fs=require('fs');
@@ -16,8 +17,60 @@ const servidor=http.createServer((pedido ,respuesta) => {
     const objetourl = url.parse(pedido.url);
     let camino='public'+objetourl.pathname;
     if (camino=='public/')
-        camino='public/index.html';
+        camino='public/home.html';
     encaminar(pedido,respuesta,camino);
 });
 
 servidor.listen(8888);
+
+function encaminar (pedido,respuesta,camino) {
+    console.log(camino);
+    switch (camino) {
+        case 'public/recuperardatos': {
+            recuperar(pedido,respuesta);
+            break;
+        }
+        default : {
+            fs.stat(camino, error => {
+                if (!error) {
+                    fs.readFile(camino,(error, contenido) => {
+                        if (error) {
+                            respuesta.writeHead(500, {'Content-Type': 'text/plain'});
+                            respuesta.write('Error interno');
+                            respuesta.end();
+                        } else {
+                            const vec = camino.split('.');
+                            const extension=vec[vec.length-1];
+                            const mimearchivo=mime[extension];
+                            respuesta.writeHead(200, {'Content-Type': mimearchivo});
+                            respuesta.write(contenido);
+                            respuesta.end();
+                        }
+                    });
+                } else {
+                    respuesta.writeHead(404, {'Content-Type': 'text/html'});
+                    respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>');
+                    respuesta.end();
+                }
+            });
+        }
+    }
+}
+
+
+function recuperar(pedido,respuesta) {
+    let info = '';
+    pedido.on('data', datosparciales => {
+        info += datosparciales;
+    });
+    pedido.on('end', () => {
+        const formulario = querystring.parse(info);
+        respuesta.writeHead(200, {'Content-Type': 'text/html'});
+        const pagina=
+            `<!doctype html><html><head></head><body>
+    Comentario${formulario['comentario']}<br>
+    <a href="home.html">Retornar</a>
+    </body></html>`;
+        respuesta.end(pagina);
+    });
+}
