2216d3d0695371763e7cd9c70d7dc5c5720de27e
pplim <ppm404@inlumine.ual.es>
2022-04-24 15:00:30 +0200

    ya guarda el opinion.txt una vez introducimos un comentario y le damos a grabar opinion para la IA

--- cajaOpiniones.js
+++ cajaOpiniones.js
@@ -4,6 +4,8 @@ const http=require('http');
 const url=require('url');
 const fs=require('fs');
 const querystring = require('querystring');
+let info;
+
 
 const mime = {
     'html' : 'text/html',
@@ -15,6 +17,7 @@ const mime = {
 };
 
 const servidor=http.createServer((pedido ,respuesta) => {
+
     const objetourl = url.parse(pedido.url);
     let camino='public'+objetourl.pathname;
     if (camino=='public/')
@@ -28,10 +31,45 @@ console.log('Servidor corriendo en http://localhost:8888')
 function encaminar (pedido,respuesta,camino) {
     console.log(camino);
     switch (camino) {
-        case 'public/recuperardatos': {
+       /*case 'public/recuperardatos': {
             recuperar(pedido,respuesta);
             break;
-        }
+        }*/
+        case 'public/grabaropinion':{
+            console.log('Vamos a grabar la opinion en el archivo opinion.txt');
+            //Procesamiento de los datos de los formularios
+
+            info=''; //Una variable auxiliar
+            pedido.on('data', tmp => {
+                info+=tmp;
+
+            }); // Un evento que copia lo que viene desde un formulario en la variable info
+
+            pedido.on('end',() =>{ // La escritura del archivo dentro del evento: Idea feliz
+                let datosformulario;
+                datosformulario=querystring.parse(info); // Se guarda en un formato "preguntable" desde javascript
+                let opinionf;
+                opinionf=datosformulario['comentario']; // el formato es nombrevar=contenido&nombrevar2=contenido&...
+
+                fs.writeFile('opinion.txt',opinionf,{ flag: 'w+' }, err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    else{
+                        console.log('Archivo correctamente escrito')
+                    }
+                    //file written successfully
+                    console.log(opinionf) //Esto es para ver que funciona
+                    console.log(info) // funciona
+                    console.log(datosformulario['opinion']); // y vuelve a funcionar...
+                } )
+            });
+            let pagina;
+            pagina='public/home.html'; // en pagina principal hay un codigo html. Ahora os lo enseño.
+            /////
+            break;
+        };
         default : {
             fs.stat(camino, error => {
                 if (!error) {
@@ -71,6 +109,7 @@ function recuperar(pedido,respuesta) {
         const pagina=
             `<!doctype html><html><head></head><body>
     Comentario: ${formulario['comentario']}<br>
+    
     <a href="home.html">Retornar</a>
     </body></html>`;
         respuesta.end(pagina);
