6c3a559ca8091dee1228bbfce2cbb5f5b16a6124
pplim <ppm404@inlumine.ual.es>
2022-05-03 20:37:56 +0200

    completo

--- cajaOpiniones.js
+++ cajaOpiniones.js
@@ -4,6 +4,8 @@ const http=require('http');
 const url=require('url');
 const fs=require('fs');
 const querystring = require('querystring');
+const {run} = require("node-matlab");
+
 let info;
 
 
@@ -28,9 +30,7 @@ const servidor=http.createServer((pedido ,respuesta) => {
 servidor.listen(8888);
 console.log('Servidor corriendo en http://localhost:8888')
 
-function spawn(mat, param2) {
-    return undefined;
-}
+
 
 function encaminar (pedido,respuesta,camino) {
     console.log(camino);
@@ -38,72 +38,7 @@ function encaminar (pedido,respuesta,camino) {
     switch (camino) {
         case 'public/versentimiento': {
             console.log('Vamos a recuperar el archivo sentimiento.txt');
-/*
-            const matlab = require("node-matlab");
-
-            matlab
-                .run("C:\\Users\\pplim\\Downloads\\JT_lstm_prod2.m")
-                .then((result) => console.log(result))
-                .catch((error) => console.log(error));*/
-
-                fs.readFile('C:\\Users\\pplim\\Downloads\\sentimiento.txt', function (err, data) {
-                if (err) {
-                    throw err;
-                }else{
-                    console.log('Archivo correctamente leido')
-                }
-                console.log(data.toString());
-                    respuesta.writeHead(200, {'Content-Type': 'text/html'});
-                    respuesta.write(data);
-                    return respuesta.end();
-            });
-                break;
-        }
-        case 'public/grabaropinion':{
-            console.log('Vamos a grabar la opinion en el archivo opinion.txt');
-            //Procesamiento de los datos de los formularios
-
-            info=''; //Una variable auxiliar
-            pedido.on('data', tmp => {
-                info+=tmp;
-
-            }); // Un evento que copia lo que viene desde un formulario en la variable info
-
-            pedido.on('end',() =>{ // La escritura del archivo dentro del evento: Idea feliz
-                let datosformulario;
-                datosformulario=querystring.parse(info); // Se guarda en un formato "preguntable" desde javascript
-                let opinionf;
-                opinionf=datosformulario['comentario']; // el formato es nombrevar=contenido&nombrevar2=contenido&...
-
-                fs.writeFile('C:\\Users\\pplim\\Downloads\\opinion.txt',opinionf,{ flag: 'w+' }, err => {
-                    /*try {
-                        const data = fs.readFileSync('C:\\Users\\pplim\\Downloads\\JT_lstm_prod2', 'utf8');
-                        console.log(data.toString());
-                    } catch (err) {
-                        console.error(err);
-                    }*/
-                    if (err) {
-                        console.error(err)
-                        return
-                    }
-                    else{
-                        console.log('Archivo correctamente escrito')
-                    }
-                    //file written successfully
-                    console.log(opinionf) //Esto es para ver que funciona
-                    console.log(info) // funciona
-                    console.log(datosformulario['opinion']); // y vuelve a funcionar...
-                } )
-            });
-            let pagina;
-            pagina='public/home.html'; // en pagina principal hay un codigo html. Ahora os lo enseño.
-            /////
-            break;
-        };
-
-        case 'public/showopinion': {
-            console.log('Vamos a recuperar el archivo opinion.txt');
-            fs.readFile('C:\\Users\\pplim\\Downloads\\opinion.txt', function (err, data) {
+            fs.readFile('C:\\Users\\pplim\\Downloads\\sentimiento.txt', function (err, data) {
                 if (err) {
                     throw err;
                 }else{
@@ -116,7 +51,72 @@ function encaminar (pedido,respuesta,camino) {
             });
             break;
         }
+        case 'public/grabaropinion':{
+            console.log('Vamos a grabar la opinion en el archivo opinion.txt');//Procesamiento de los datos de los formularios
 
+            info=''; //Una variable auxiliar
+            pedido.on('data', tmp => {
+                info+=tmp;
+            }); // Un evento que copia lo que viene desde un formulario en la variable info
+
+            pedido.on('end',() =>{ // La escritura del archivo dentro del evento: Idea feliz
+                let datosformulario;
+                datosformulario=querystring.parse(info); // Se guarda en un formato "preguntable" desde javascript
+                let opinionf;
+                opinionf=datosformulario['comentario']; // el formato es nombrevar=contenido&nombrevar2=contenido&...
+
+                fs.writeFile('C:\\Users\\pplim\\Downloads\\opinion.txt',opinionf,{ flag: 'w+' }, err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    else{
+                        console.log('Archivo correctamente escrito')
+                    }
+                    //file written successfully
+                    console.log(opinionf) //Esto es para ver que funciona
+
+                } )
+            });
+            break;
+        };
+
+        case 'public/showopinion': {
+            console.log('Vamos a recuperar el archivo opinion.txt');
+            let datosformulario;
+            datosformulario=querystring.parse(info); // Se guarda en un formato "preguntable" desde javascript
+            let opinionf;
+            opinionf=datosformulario['comentario'];
+
+                console.log(opinionf);
+                respuesta.writeHead(200, {'Content-Type': 'text/html'});
+                respuesta.write(opinionf);
+                return respuesta.end();
+
+            break;
+        }
+        case 'public/guardarDB': {
+            console.log('Guardar la opinion y el sentimiento en una txt');
+            let datosformulario;
+            datosformulario = querystring.parse(info); // Se guarda en un formato "preguntable" desde javascript
+            let opinionf;
+            opinionf = datosformulario['comentario'];
+                 fs.readFile('C:\\Users\\pplim\\Downloads\\sentimiento.txt', function (err, data) {
+                        if (err) {
+                            throw err;
+                        } else {
+                            console.log(data.toString());
+                            fs.appendFile('nodep1bd.txt',opinionf+ "-" +data.toString(), err => {
+                                if (err) throw err;
+                                else {
+                                    console.log('Archivo correctamente escrito');
+                                }
+                            })
+
+                        }
+                    })
+            break;
+                }
 
         default : {
             fs.stat(camino, error => {
